7ccd581b2c533ca93d18ad82004d3846e2eb4512
AdamBrown0 <adam@djb.org.uk>
2024-06-15 03:06:22 +0100

    Fix Listen annotation to work with single and multiple mixins

--- src/main/java/com/github/spaghettifying/elysium/util/TickByMixin.java
+++ src/main/java/com/github/spaghettifying/elysium/util/TickByMixin.java
@@ -18,10 +18,24 @@ public class TickByMixin {
     private static HashMap<Class<?>, ArrayList<Hack>> hacksPerMixin = new HashMap<>();
 
     static {
-        Set<Class<?>> classesListening = new Reflections("com.github.spaghettifying.elysium.hack.impl").getTypesAnnotatedWith(Listeners.class);
-        for (Class<?> clazz : classesListening) {
+        Set<Class<?>> classesListeningSingle = new Reflections("com.github.spaghettifying.elysium.hack.impl").getTypesAnnotatedWith(Listen.class);
+        for (Class<?> clazz : classesListeningSingle) {
+            Listen annotation = clazz.getAnnotation(Listen.class);
+            if (!hacksPerMixin.containsKey(annotation.value()))
+                hacksPerMixin.put(annotation.value(), new ArrayList<>());
+
+            ArrayList<Hack> list = hacksPerMixin.get(annotation.value());
+
+            for (Hack hack : Elysium.getHackRegistry().getHacks().keySet())
+                if (hack.getClass().equals(clazz))
+                    list.add(hack);
+        }
+
+        Set<Class<?>> classesListeningMultiple = new Reflections("com.github.spaghettifying.elysium.hack.impl").getTypesAnnotatedWith(Listeners.class);
+        for (Class<?> clazz : classesListeningMultiple) {
             Listeners listenersAnnotation = clazz.getAnnotation(Listeners.class);
             for (Listen listenAnnotation : listenersAnnotation.value()) {
+                System.out.println(listenAnnotation.value());
                 if (!hacksPerMixin.containsKey(listenAnnotation.value()))
                     hacksPerMixin.put(listenAnnotation.value(), new ArrayList<>());
 
